85d047ea9713a7db7b5cf4d1fc50de2807623eb3
Jorge <jlcallalle@medialabla.com>
2019-02-18 16:41:34 -0500

    update oligume

--- html/skin/frontend/default/lamejornaranjav2/js/regalar-cajas-de-naranjas/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-	var boton = $('#boton-empresas');
-	boton.on('click', function(event){
-		event.preventDefault();
-		var selectedHref = $(this).attr('href'),
-			target= $(selectedHref);
-		$('body,html').animate({ 'scrollTop': target.offset().top - 0}, 900);
-	})
-});
